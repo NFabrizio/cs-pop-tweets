43466fc4bd4757264d6731dfa5e4b814d1f7aa29
NFabrizio <nick.fabrizio@pearson.com>
2022-03-06 15:01:21 -0700

    feature/remaining-bolts Adds remaining bolts and updates report bolt and topology to include them

--- src/main/java/org/poptweets/PopTweetsTopology.java
+++ src/main/java/org/poptweets/PopTweetsTopology.java
@@ -1,24 +1,33 @@
 package main.java.org.poptweets;
 
+import main.java.org.poptweets.bolt.HashTagFilterBolt;
 import main.java.org.poptweets.bolt.ReportBolt;
+import main.java.org.poptweets.bolt.TweetSplitBolt;
 import main.java.org.poptweets.spout.TwitterSpout;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 
 public class PopTweetsTopology {
     private static final String TOPOLOGY_NAME = "popular-tweets-topology";
     private static final String TWITTER_SPOUT_ID = "twitter-spout";
+    private static final String TWEET_SPLIT_BOLT_ID = "tweet-split-bolt";
+    private static final String HASH_TAG_FILTER_BOLT_ID = "hash-tag-filter-bolt";
     private static final String REPORT_BOLT_ID = "report-bolt";
 
     public static void main(String[] args) throws Exception {
         TwitterSpout spout = new TwitterSpout();
+        TweetSplitBolt tweetSplitBolt = new TweetSplitBolt();
+        HashTagFilterBolt hashTagFilterBolt = new HashTagFilterBolt();
         ReportBolt reportBolt = new ReportBolt();
 
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout(TWITTER_SPOUT_ID, spout);
-        builder.setBolt(REPORT_BOLT_ID, reportBolt).shuffleGrouping(TWITTER_SPOUT_ID);
+        builder.setBolt(TWEET_SPLIT_BOLT_ID, tweetSplitBolt).shuffleGrouping(TWITTER_SPOUT_ID);
+        builder.setBolt(HASH_TAG_FILTER_BOLT_ID, hashTagFilterBolt).fieldsGrouping(TWEET_SPLIT_BOLT_ID, new Fields("word"));
+        builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(HASH_TAG_FILTER_BOLT_ID);
 
         Config config = new Config();
         if (args.length == 1) {
---
+++ src/main/java/org/poptweets/bolt/HashTagFilterBolt.java
@@ -0,0 +1,32 @@
+package main.java.org.poptweets.bolt;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import java.util.Map;
+
+public class HashTagFilterBolt extends BaseRichBolt {
+    private OutputCollector collector;
+
+    public void prepare(Map config, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    public void execute(Tuple tuple) {
+        String word = tuple.getStringByField("word");
+//        String[] words = tweet.split(" ");
+
+        if (word != null && !word.trim().isEmpty() && word.charAt(0) == '#') {
+            this.collector.emit(new Values(word));
+        }
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("hashTag"));
+    }
+}
