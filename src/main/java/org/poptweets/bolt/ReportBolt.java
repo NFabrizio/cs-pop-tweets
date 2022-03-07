ea7c24946e0d7dbf7019d16d141f93d4677dfe9b
NFabrizio <nicholas.fabrizio@gmail.com>
2022-03-06 22:12:00 -0700

    feature/integrating-lossy-counting Fixes issues with data not being passed from LossyCountingBolt

--- src/main/java/org/poptweets/bolt/ReportBolt.java
+++ src/main/java/org/poptweets/bolt/ReportBolt.java
@@ -5,6 +5,7 @@ import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
+import org.apache.storm.utils.Utils;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -14,6 +15,7 @@ import java.util.*;
 public class ReportBolt extends BaseRichBolt {
 //    private HashMap<String, Long> counts = null;
     private ArrayList<String> hashTags = null;
+//    private LinkedHashMap<String, Integer> hashTags = new LinkedHashMap<String, Integer>();
     private String logPath = "TwitterSpoutLog.txt";
     private String time;
 
@@ -33,8 +35,8 @@ public class ReportBolt extends BaseRichBolt {
 //        String hashTag = tuple.getStringByField("hashTag");
 //        this.hashTags.add(hashTag);
         String hashTag = tuple.getStringByField("tag");
-        System.out.println("tuple.getLongByField(time)");
-        System.out.println(tuple.getLongByField("time"));
+//        System.out.println("tuple.getLongByField(time)");
+//        System.out.println(tuple.getLongByField("time"));
         this.time = String.valueOf(tuple.getLongByField("time"));
         this.hashTags.add(hashTag);
     }
@@ -63,30 +65,54 @@ public class ReportBolt extends BaseRichBolt {
         };
         BufferedWriter bw = new BufferedWriter(fileWriter);
 
-        try {
+//        if (this.time != null) {
+            try {
+                Utils.sleep(1000 * 10);
 //            System.out.println("this.time: ");
 //            System.out.println(this.time);
-            bw.write(this.time);
-//            bw.write(String.valueOf(System.currentTimeMillis()));
-        } catch (IOException e) {
-            System.out.println("Error occurred while attempting to write time");
-            e.printStackTrace();
-        }
+                bw.write(this.time);
+                System.out.println("hashTags.size()");
+                System.out.println(this.hashTags.size());
 
-        for (String hashTag : hashTags){
-            System.out.println(hashTag);
-            try {
-                bw.write(hashTag);
+                int index = 0;
+                int longestIndex = 0;
+                for (String tagList : this.hashTags) {
+                    String[] tags = tagList.split(",");
+                    if (tags.length > longestIndex) {
+                        longestIndex = index;
+                    }
+                    index++;
+                }
+//                int hashTagsLength = hashTags.size();
+//                int hashTagsLastIndex = hashTags.size() - 1;
+//                bw.write(hashTags.get(hashTagsLastIndex));
+//                bw.write(this.hashTags.get(this.hashTags.size() - 1));
+                bw.write(this.hashTags.get(longestIndex));
                 bw.newLine();
+//            bw.write(String.valueOf(System.currentTimeMillis()));
+            } catch (IOException e) {
+                System.out.println("Error occurred while attempting to write logs");
+                e.printStackTrace();
+            }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+
+        for (String hashTag : hashTags) {
+                System.out.println(hashTag);
+//                try {
+//                    bw.write(hashTag);
+//                    bw.newLine();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+            }
+
+            try {
+                bw.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-
-        try {
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        }
     }
 }
